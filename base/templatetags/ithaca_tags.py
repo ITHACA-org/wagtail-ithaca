4fe36b55c28782fa991719f7d4e90cdf8e5aacf9
ppasq <paolo.pasquali@gmail.com>
2020-03-11 12:59:34 +0100

    Add blog posts in home page

--- base/templatetags/ithaca_tags.py
+++ base/templatetags/ithaca_tags.py
@@ -6,12 +6,12 @@ from people.models import PersonPage
 
 register = template.Library()
 
-# Blog feed for home page
-@register.inclusion_tag('ithaca/tags/homepage_blog_listing.html', takes_context=True)
+
+@register.inclusion_tag('tags/homepage_blog_listing.html', takes_context=True)
 def homepage_blog_listing(context, count=6):
-    blog_posts = BlogPage.objects.live().in_menu().order_by('-date')[:count]
+    posts = BlogPage.objects.live().order_by('-date')[:count]
     return {
-        'blog_posts': blog_posts,
+        'posts': posts,
         # required by the pageurl tag that we want to use within this template
         'request': context['request'],
     }
