10e3857ac6f434740d63ccfbf432a46b5ca510e9
ppasq <paolo.pasquali@gmail.com>
2020-04-07 15:50:36 +0200

    Order projects in home page

--- base/templatetags/ithaca_tags.py
+++ base/templatetags/ithaca_tags.py
@@ -19,7 +19,7 @@ def homepage_blog_listing(context, count=3):
 
 @register.inclusion_tag('tags/homepage_projects_listing.html', takes_context=True)
 def homepage_projects_listing(context, count=4):
-    projects = ProjectPage.objects.filter(live=True)[:count]
+    projects = ProjectPage.objects.filter(live=True).order_by('pk')[:count]
     return {
         'projects': projects,
         # required by the pageurl tag that we want to use within this template
