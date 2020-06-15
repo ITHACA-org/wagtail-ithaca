c8843a90c421ad96131762aca2b9ab386a94f625
ppasq <paolo.pasquali@gmail.com>
2020-06-15 10:27:54 +0200

    Clean up

--- ithaca/settings/production.py
+++ ithaca/settings/production.py
@@ -2,16 +2,6 @@ from .base import *
 
 DEBUG = False
 
-INSTALLED_APPS += (
-    'wagtail.contrib.postgres_search',
-)
-
-WAGTAILSEARCH_BACKENDS = {
-    'default': {
-        'BACKEND': 'wagtail.contrib.postgres_search.backend',
-    },
-}
-
 TAGGIT_CASE_INSENSITIVE = True
 
 try:
